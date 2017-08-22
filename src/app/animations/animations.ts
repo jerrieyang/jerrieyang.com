1172284824b0936af4f9189182db57e3424d0554
jerrie.yang <zhenyuan.yang@adesa.com>
2017-08-22 10:09:16 -0400

    almost there

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -22,7 +22,10 @@ export const fadeInOut: AnimationTriggerMetadata =  trigger('fadeInOut', [
 ]);
 
 export const fadeIn: AnimationTriggerMetadata =  trigger('fadeIn', [
-  state('*', style({ 'opacity': 1, transform: 'translateY(0)'})),
-  state('void', style({ 'opacity': 0,  transform: 'translateY(10px)' })),
+  state('*', style({ 'opacity': 1})),
+  state('void', style({ 'opacity': 0 })),
   transition('void => *', animate(300))
 ]);
+//, transform: 'translateY(0)
+//,  transform: 'translateY(10px)'
+
