aa7147fa5da8feb1b6b8e6fb1e3b20409c51a18b
jerrie.yang <zhenyuan.yang@adesa.com>
2017-08-22 22:08:12 -0400

    polyfill update

--- src/app/components/recruiters/recruiters.component.ts
+++ src/app/components/recruiters/recruiters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-recruiters',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recruiters.component.css']
 })
 export class RecruitersComponent implements OnInit {
+  resumeSource = environment.assetsUrl + '/jerrieyang_resume.rtf.zip';
 
   constructor() { }
 
